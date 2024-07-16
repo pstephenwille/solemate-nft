01d41da2ebd00957efd139e9d4fe7bf0092f0b6e
stephen.wille <stephen.wille@climate.com>
2024-07-16 15:47:32 -0700

    misc

--- test/NFT.t.sol
+++ test/NFT.t.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
 import {NFT} from "../src/NFT.sol";
 
 contract CounterTest is Test {
-    NFT public counter;
+     NFT public  counter;
 
     function setUp() public {
-        counter = new NFT('sille', 'SWILLE');
+        counter =  new NFT('swille', 'SWILLE');
+    }
+
+    function test_checkTokenName() public view {
+        string memory tokenName = counter.name();
+        assertEq('swille', tokenName);
     }
 
 }
